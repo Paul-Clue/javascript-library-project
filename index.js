c0979628f234d4dd0bb19a4b874595e8681a143e
unknown <paulclue20@gmail.com>
2021-05-05 17:10:21 -0500

    Update addBookToLibrary function

--- index.js
+++ index.js
@@ -6,24 +6,31 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
-// function addBookToLibrary(book) {
-//   if(typeof book != Book){
-//     book = null;
-//   }else{
-//     myLibrary.push(book);
-//   }
-// }
-
-function addBookToLibraryinList (form) {
+function addBookToLibrary (form) {
   let tle = form.title.value;
   let athr = form.author.value;
   let pges = form.pages.value;
-  console.log(tle, athr, pges);
-  alert(new Book(tle, athr, pges));
+  b1 = new Book(tle, athr, pges);
+  myLibrary.push(b1);
 
-  // addBookToLibrary(
-  // myLibrary.push(new Book(tle, athr, pges));
-  // console.log(myLibrary);
+  if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem(myLibrary, JSON.stringify(myLibrary));
 
+    // Retrieve
+    let lib = JSON.parse(localStorage.getItem("names"));
+    document.getElementById("show").innerHTML = localStorage.getItem(lib);
+  } else {
+    document.getElementById("show").innerHTML = "Sorry, your browser does not support Web Storage...";
+  }
+  
+  
 }
-// [[t , a, p]]
+console.log(myLibrary.length);
+// var names = [];
+// names[0] = prompt("New member name?");
+// localStorage.setItem("names", JSON.stringify(names));
+// var storedNames = JSON.parse(localStorage.getItem("names"));
+
+
+
