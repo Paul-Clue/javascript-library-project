8f92a0a489d5aa20f1b58b3787086cbbc91744a2
abdulkhaliqdev <abdulkhaliql1f18bscs0159@gmail.com>
2021-05-06 02:12:51 +0500

    Add form to get new book data

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
-function addBookToLibrary(book) {
-  if(typeof book != Book){
-    book = null;
-  }else{
-    myLibrary.push(book);
-  }
+// function addBookToLibrary(book) {
+//   if(typeof book != Book){
+//     book = null;
+//   }else{
+//     myLibrary.push(book);
+//   }
+// }
+
+function addBookToLibraryinList (form) {
+  let tle = form.title.value;
+  let athr = form.author.value;
+  let pges = form.pages.value;
+  console.log(tle, athr, pges);
+  alert(new Book(tle, athr, pges));
+
+  // addBookToLibrary(
+  // myLibrary.push(new Book(tle, athr, pges));
+  // console.log(myLibrary);
+
 }
+// [[t , a, p]]
