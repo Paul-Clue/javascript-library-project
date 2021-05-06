6d7b08d66e5691521ae76050e9d0b57cf459b3b5
abdulkhaliqdev <abdulkhaliql1f18bscs0159@gmail.com>
2021-05-06 19:34:09 +0500

    save form data in library list

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
 let myLibrary = [];
-
-function Book(title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
+class Book {
+  constructor(author, title, pages) {
+      this.author = author;
+      this.title = title;
+      this.pages = pages;
+  };
 }
-
 function addBookToLibrary (form) {
   let tle = form.title.value;
   let athr = form.author.value;
   let pges = form.pages.value;
   b1 = new Book(tle, athr, pges);
   myLibrary.push(b1);
-
-  if (typeof(Storage) !== "undefined") {
-    // Store
-    localStorage.setItem(myLibrary, JSON.stringify(myLibrary));
-
-    // Retrieve
-    let lib = JSON.parse(localStorage.getItem("names"));
-    document.getElementById("show").innerHTML = localStorage.getItem(lib);
-  } else {
-    document.getElementById("show").innerHTML = "Sorry, your browser does not support Web Storage...";
-  }
-  
-  
 }
-console.log(myLibrary.length);
-// var names = [];
-// names[0] = prompt("New member name?");
-// localStorage.setItem("names", JSON.stringify(names));
-// var storedNames = JSON.parse(localStorage.getItem("names"));
 
+function displayLibrary () {
+}
 
 
